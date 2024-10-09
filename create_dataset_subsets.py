5d1dde7b601322fe6d6452109506b375390e1147
Sheharyar Ahmad <sheharyar.572@gmail.com>
2024-10-09 17:28:40 +0500

    Added new arguments for command line interface. Fixed file name bug

--- create_dataset_subsets.py
+++ create_dataset_subsets.py
@@ -3,23 +3,23 @@ import shutil
 import argparse
 
 
-def get_file_name(file_name: str, file_prefix: str, num_files: int) -> str:
-    if num_files == 1:
+def get_file_name(file_name: str, file_prefix: str, files_count: int) -> str:
+    if files_count == 1:
         return file_prefix + ".parquet"
     file_name = file_name.split("of-")[0]
-    return file_name + "of-" + str(num_files).zfill(2) + ".parquet"
+    return file_name + "of-" + str(files_count).zfill(2) + ".parquet"
 
-def create_subsets(base_dir, file_prefix, subset_prefix, step_size):
+def create_subsets(base_dir, save_dir_path, subset_prefix, file_prefix, step_size):
     files = sorted([f for f in os.listdir(base_dir) if f.startswith(file_prefix)])
     num_files = len(files)
     
     for i in range(1, num_files + 1):
-        subset_dir = os.path.join(base_dir, f"{subset_prefix}_{i * step_size // 1000}k")
+        subset_dir = os.path.join(save_dir_path, f"{subset_prefix}_{i * step_size // 1000}k")
         os.makedirs(subset_dir, exist_ok=True)
 
         for j in range(i):
             src_file = os.path.join(base_dir, files[j])
-            dst_file = os.path.join(subset_dir, get_file_name(files[j], file_prefix, num_files))
+            dst_file = os.path.join(subset_dir, get_file_name(files[j], file_prefix, i))
             shutil.copy(src_file, dst_file)
         src_test_file = os.path.join(base_dir, "test.parquet")
         dst_test_file = os.path.join(subset_dir, "test.parquet")
@@ -28,6 +28,8 @@ def create_subsets(base_dir, file_prefix, subset_prefix, step_size):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create subsets of Parquet files using Dask.")
     parser.add_argument("--directory", type=str, help="Path to the directory containing Parquet files.")
+    parser.add_argument("--save-dir-path", type=str, help="Directory path where data will be saved")
+    parser.add_argument("--dataset-name-prefix", type=str, help="Name prefix of the folder where each subset will be saved.")
     parser.add_argument("--is-shuffled", type=bool, help="Whether the files are shuffled or not.")
     args = parser.parse_args()
 
@@ -36,8 +38,17 @@ if __name__ == "__main__":
         if args.is_shuffled
         else "train"
     )
-    subset_prefix = "subset"
+    subset_prefix = (
+        args.dataset_name_prefix
+        if args.dataset_name_prefix
+        else "openai"
+    )
+    save_dir_path = (
+        args.save_dir_path
+        if args.save_dir_path
+        else args.directory
+    )
     step_size = 500_000  # 500k
 
-    create_subsets(args.directory, file_prefix, subset_prefix, step_size)
+    create_subsets(args.directory, save_dir_path, subset_prefix, file_prefix, step_size)
     print(f'Finished creating subsets of Parquet files in {args.directory}.')
