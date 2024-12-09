2b733c61acef2b68398e97d06c556ca0a5eb6a92
Sheharyar Ahmad <sheharyar.572@gmail.com>
2024-12-10 02:45:55 +0500

    updated run_churn_case.sh

--- run_churn_case.sh
+++ run_churn_case.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Run the first Python script using nohup and save the process ID
-echo "Activating virtual environment..."
-source VectorDBBench/venv/bin/activate
-echo "Virtual environment activated"
+
 
 echo "Changing directory to VectorDBBench..."
 cd VectorDBBench/
 echo "Current directory: $(pwd)"
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "Virtual environment activated"
 
 host="172.17.0.2"
 dbuser="postgres"
@@ -15,34 +17,47 @@ dbpass="admin123"
 dbname="ann"
 
 echo "Running vectordbbench with specified parameters..."
-vectordbbench pgvectorhnsw --user-name $dbuser --password $dbpass --host $host --db-name $dbname --drop-old --load --skip-search-serial --skip-search-concurrent --skip-search-churn --case-type Performance1536D500K --maintenance-work-mem 8GB --max-parallel-workers 7 --num-concurrency 1 --concurrency-duration 30 --m 16 --ef-construction 128 --ef-search 64 > ../build-index-out.log 2>&1 &
+vectordbbench pgvectorhnsw --user-name $dbuser --password $dbpass --host $host --db-name $dbname --drop-old --load --skip-search-serial --skip-search-concurrent --case-type Performance1536D500K --maintenance-work-mem 8GB --max-parallel-workers 7 --num-concurrency 1 --concurrency-duration 30 --m 32 --ef-construction 128 --ef-search 40 > ../build-index-out.log 2>&1 &
 VECTORDDBENCH_PID=$!
 echo "vectordbbench started with PID $VECTORDDBENCH_PID"
 
 echo "Waiting for index build to complete..."
 wait $VECTORDDBENCH_PID
-echo "index build completed completed"
+echo "index build completed"
+echo "deactivating virtual environment..."
+deactivate
 
-kill $VECTORDDBENCH_PID
-echo "vectordbbench index build process killed"
 
-echo "Changing directory back to the original..."
-cd ../
+echo "Changing directory back to the other VectorDBBench-churn..."
+cd ../VectorDBBench-churn/
 echo "Current directory: $(pwd)"
 
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "Virtual environment activated"
 
 echo "Starting run-churn.py with nohup..."
-nohup python3 VectorDBBench-churn/run-churn.py > churn-run-output.log  2>&1 &
+nohup python3 run-churn.py > ../churn-run-output.log  2>&1 &
 NOHUP_PID=$!
 echo "run-churn.py started with PID $NOHUP_PID"
+echo "deactivating virtual environment..."
+deactivate
 
-# Wait for 15 seconds
-echo "Sleeping for 15 seconds..."
-sleep 15
+# Wait for 100 seconds
+echo "Sleeping for 100 seconds..."
+sleep 100
 
+# deactivate
 # Run the second Python script in parallel
+cd ../VectorDBBench/
+echo "Current directory: $(pwd)"
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+echo "Virtual environment activated"
+
 echo "Starting run.py..."
-python3 VectorDBBench/run.py > qps-run-output.log  2>&1 & 
+python3 run.py > ../qps-run-output.log  2>&1 & 
 RUN_PY_PID=$!
 echo "run.py started with PID $RUN_PY_PID"
 
@@ -50,6 +65,11 @@ echo "run.py started with PID $RUN_PY_PID"
 echo "Waiting for run.py to complete..."
 wait $RUN_PY_PID
 echo "run.py completed"
+echo "deactivating virtual environment..."
+deactivate
+
+cd ../
+echo "Current directory: $(pwd)"
 
 # Kill the nohup process
 echo "Killing run-churn.py with PID $NOHUP_PID..."
